8b482b5a627b841c33ae658a37a813c40f02d72f
Dominik Brandl <dbrandl72@gmail.com>
2014-06-20 22:04:22 +0200

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates the cacheable input for the cacheSolve 
+## function. 
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions which can be used to 
+## access the data and the cached result
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        i <<- NULL
+        x <<- y
+    }
+    get <- function() x
+    setInverse <- function(inverse) i <<- inverse
+    getInverse <- function() i
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve tries to get the cached result from the input and, if 
+## the cache is empty, calculates the result and fills the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setInverse(i)
+    i
 }
